23bd7d31586c32741d14acf37fabbff5d292737f
Morgan Byrne <morganlbyrne@gmail.com>
2021-10-12 19:50:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.body.remove('main');
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Morgan is the champion";
+
+
+
+
+
+
